5ce4e97b0e934308a0516b3296bbd853c0b429e2
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-30 09:23:06 +0100

    config i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+  end
 end
