120a97c810e77c15a0cea344637e53d0f9ffddd6
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-30 15:29:57 +0100

    controller de works

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,28 @@
+class WorksController < ApplicationController
+  def index
+    if params[:collection].nil?
+      @works = Work.all
+    else
+      @works = Work.where(collection: params[:collection]).shuffle
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:name, :description, :dimensions, :year, :image, :category, :collection, :photo)
+  end
+end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,12 @@
 class Work < ApplicationRecord
   has_one_attached :photo
+
+  CATEGORIES = ["Kinetics", "Statics"]
+  COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
+
+  # validates :category, presence: true, inclusion: { in: CATEGORIES }
+  # validates :collection, presence: true, inclusion: { in: COLLECTIONS }
+  # validates :name, presence: true
+  # validates :year, presence: true
+  # validates :dimensions, presence: true
 end
