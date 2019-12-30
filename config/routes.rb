4cb850869d9931a7dd6f387e465e3dd7739053d9
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-30 09:49:48 +0100

    default locale = fr / test in home view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   scope '(:locale)', locale: /fr|en/ do
     root to: 'pages#home'
   end
