9479610924be43bbd801bb6124eef49b8f947b07
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-06 17:39:48 +0100

    restored inital set up of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,30 +22,44 @@ csv.each do |row|
   t.description = row['description']
   t.dimensions = row['dimensions']
   t.year = row['year']
-  t.local_image = row['local_image']
+  t.image = row['image']
   t.youtube_link = row['youtube_link']
   t.category = row['category']
   t.collection = row['collection']
-  t.cloud_image = row['cloud_image']
+  # t.cloud_image = row['cloud_image']
   t.save
   puts "#{t.name}, #{t.dimensions} saved"
 end
 puts "Now, there are #{Work.count} saved via seed in tha Database"
 
-# This was for uploading the 100 local photos to cloudinary and link each one to its instance in DB
-# Then we need to link existing cloudinary photo.key to our instances in DB for production
+
 works = Work.all
 works.each do |work|
-  picture = Cloudinary::Uploader.upload("#{work.local_image}")
+  picture = Cloudinary::Uploader.upload("#{work.image}")
   puts "image put on Cloudinary"
   url = URI.open(picture["secure_url"])
   p url.class
   work.photo.attach(io: url, filename: work.name, content_type: 'image/jpg')
   p work.photo.attached?
-  work.cloud_image = work.photo.key
   work.save
   puts "Now, #{work.name} should have a picture"
 end
+
+
+# This was for uploading the 100 local photos to cloudinary and link each one to its instance in DB
+# Then we need to link existing cloudinary photo.key to our instances in DB for production
+# works = Work.all
+# works.each do |work|
+#   picture = Cloudinary::Uploader.upload("#{work.local_image}")
+#   puts "image put on Cloudinary"
+#   url = URI.open(picture["secure_url"])
+#   p url.class
+#   work.photo.attach(io: url, filename: work.name, content_type: 'image/jpg')
+#   p work.photo.attached?
+#   work.cloud_image = work.photo.key
+#   work.save
+#   puts "Now, #{work.name} should have a picture"
+# end
 
 # migration pour modifier colonne image en local_image
 # migration pour ajouter colonne cloud_image
