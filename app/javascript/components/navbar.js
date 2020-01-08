12b4468c7afa19d98703a14f23efb789d00eff47
Benjamin Leray <b.leray35@gmail.com>
2020-01-08 17:15:56 +0100

    added swipe animation for mobile menu, and background on mobile version for burger button

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -15,10 +15,15 @@ const hideOnClikMenu = () => {
   const burger = document.querySelector(".burger");
   const background = document.querySelector(".background-mobile");
   const about = document.querySelector(".mobile-about");
+  const menuItems = document.querySelectorAll(".mobile-item");
 
   burger.addEventListener("click", () => {
-    background.classList.toggle("inactive");
-    about.classList.toggle("inactive");
+    background.classList.toggle("menu-active");
+    about.classList.toggle("menu-active");
+    burger.classList.toggle("menu-active");
+    menuItems.forEach((item) =>{
+      item.classList.toggle("menu-active");
+    });
   });
 };
 
