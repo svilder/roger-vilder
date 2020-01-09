e7f3f1b342b6a4f3f0626af77e35fc869c946fe2
Benjamin Leray <b.leray35@gmail.com>
2020-01-09 11:10:10 +0100

    Fixed matter when changing locale, display all works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,29 +1,10 @@
 class WorksController < ApplicationController
   def index
-    @work = Work.new
     if params[:collection].nil?
-      @works = Work.all
+      @works = Work.where(collection: "Kinetic")
     else
-      @works = Work.where(collection: params[:collection]).shuffle
+      @works = Work.where(collection: params[:collection])
     end
-  end
-
-  def new
     @work = Work.new
-  end
-
-  def create
-    @work = Work.new(work_params)
-    if @work.save
-      redirect_to works_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def work_params
-    params.require(:work).permit(:name, :description, :dimensions, :year, :image, :youtube_link, :category, :collection, :photo)
   end
 end
