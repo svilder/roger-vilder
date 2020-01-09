21c148e72ff8bdfb31931909a12a17958981ebb5
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-09 17:52:04 +0100

    controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,11 +1,11 @@
 class WorksController < ApplicationController
   def index
-
     if params[:work_id].nil?
       @work = Work.last
     else
       @work = Work.find(params[:work_id])
     end
+
     if params[:collection].nil?
       @works = Work.where(collection: "Kinetic")
     else
