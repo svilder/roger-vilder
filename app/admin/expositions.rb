18ad6df169d5a9ca2758cf740ccc6f3dd2399980
Benjamin Leray <b.leray35@gmail.com>
2020-01-09 12:35:46 +0100

    Corrected syntax errors in French

--- app/admin/expositions.rb
+++ app/admin/expositions.rb
@@ -4,10 +4,10 @@ ActiveAdmin.register Exposition do
   form title: "Exposition" do |f|
     inputs "Exposition" do
       f.input :category, label: "Type d'exposition", collection: Exposition::EXPOSITIONS_CATEGORIES
-      f.input :title, placeholder: "Nom de l exposition"
-      f.input :year, placeholder: "2019"
-      f.input :city, placeholder: "Paris"
-      f.input :exhibition, label: "lieu d'exposition", placeholder: "Centre Pompidou"
+      f.input :title, label: "Titre de l'exposition", placeholder: "Nom de l exposition"
+      f.input :year, label: "Année", placeholder: "2019"
+      f.input :city, label: "Ville", placeholder: "Paris"
+      f.input :exhibition, label: "Lieu d'exposition", placeholder: "Centre Pompidou"
       f.input :associates, label: " Facultatif: Artistes ayant participés", placeholder: " Salvator Dali et Rembrandt"
     end
   end
