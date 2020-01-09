0614c419d13bed23c373aaa4dc818421dc2efe42
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-09 17:47:25 +0100

    Work detailed on side bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import { switchColorNavbar, hideOnClikMenu } from '../components/navbar';
+import { showTheWork } from '../components/work_card';
 
 switchColorNavbar();
 hideOnClikMenu();
+showTheWork();
