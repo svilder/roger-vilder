4d055aae3a5b33c0dba5ef3e4d87a2bd38639fd4
Benjamin Leray <b.leray35@gmail.com>
2020-01-09 14:44:48 +0100

    form for bibliographies

--- app/admin/bibliographies.rb
+++ app/admin/bibliographies.rb
@@ -2,4 +2,13 @@ ActiveAdmin.register Bibliography do
 
   permit_params :author, :title, :year, :month
 
+  form title: "Bibliographie" do |f|
+    inputs "Bibliographie" do
+      f.input :title, label: "Titre", placeholder: ""
+      f.input :author, label: "Auteur", placeholder: ""
+      f.input :year, label: "Année de publication", placeholder: "2019"
+      f.input :month, label: "Mois de publication (en lettres)", placeholder: "Janvier"
+    end
+  end
+
 end
