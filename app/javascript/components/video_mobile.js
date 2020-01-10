757aa4362b8a96ba0f6c4f4862de62bbbc60c9ef
Sandra Vilder <sandra.vilder@gmail.com>
2020-01-10 17:41:25 +0100

    card mobile - try on youtube video width and height variable

---
+++ app/javascript/components/video_mobile.js
@@ -0,0 +1,11 @@
+const videoMobile = () => {
+  const video = document.querySelector('.video');
+  const screenWidth = window.innerWidth
+  if (screenWidth <= 768) {
+
+  } else {
+
+  }
+};
+
+export {videoMobile};
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,9 @@
 import "bootstrap";
 import { switchColorNavbar, hideOnClikMenu } from '../components/navbar';
 import { showTheWork } from '../components/work_card';
+import { videoMobile } from '../components/video_mobile';
 
 switchColorNavbar();
 hideOnClikMenu();
 showTheWork();
+videoMobile();
