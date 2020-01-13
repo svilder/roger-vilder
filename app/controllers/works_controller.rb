ac4b79c4ed67cb5367f12e8e0b2811e647d6a47f
Benjamin Leray <b.leray35@gmail.com>
2020-01-13 12:57:12 +0100

    The show page works, render TODO, navigation possible

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,15 +6,14 @@ class WorksController < ApplicationController
     else
       @works = Work.where(collection: params[:collection])
     end
+
     @work = Work.first if params[:work_id].nil?
 
     # suite navigation entre les show et contacs
-    if request.referer.match(/[0-9]+$/) # <- Check if Url from request end with a number, witch means : from a show page
-      show_id = request.referer.scan(/[0-9]+$/).join # <- Extract the id from the request referer
-      @work = Work.find(show_id)
-    else
-      @work = Work.first
-    end
+    # if request.referer.match(/[0-9]+$/) # <- Check if Url from request end with a number, witch means : from a show page
+    #   show_id = request.referer.scan(/[0-9]+$/).join # <- Extract the id from the request referer
+    #   @work = Work.find(show_id)
+    # end
   end
 
   def show
