9aafec69ae56f5e3a29b8545c5221c438c4a2f40
Benjamin Leray <b.leray35@gmail.com>
2020-01-13 13:59:14 +0100

    Persistence of showed card information when navigation

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,12 +1,11 @@
 class WorksController < ApplicationController
   def index
-    # Premiere connextion a l'index
     if params[:collection].nil?
       @works = Work.where(collection: "Kinetic")
     else
       @works = Work.where(collection: params[:collection])
     end
-    @work = Work.first
+    @work = Work.last
     @work = Work.find(params[:showed_work].to_i) if params[:showed_work].present?
   end
 
