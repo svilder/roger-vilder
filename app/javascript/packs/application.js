1e51260d99df677991442ab327b8f0fda3538352
Benjamin Leray <b.leray35@gmail.com>
2020-01-13 14:29:29 +0100

    deleted old JS behavior, and applying tag to Video_tag

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,6 @@
 import "bootstrap";
 import { switchColorNavbar, hideOnClikMenu } from '../components/navbar';
-import { showTheWork } from '../components/work_card';
-import { videoMobile } from '../components/video_mobile';
 
 switchColorNavbar();
 hideOnClikMenu();
-showTheWork();
-videoMobile();
+
