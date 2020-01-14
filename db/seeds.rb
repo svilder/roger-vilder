b29a70c5455a60007ac9dcac60f8658ac351513e
Benjamin Leray <b.leray35@gmail.com>
2020-01-14 10:38:48 +0100

    added csv for personnals exhibitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts "Seed Start"
 
 
   puts "Parsing exhibition"
-  csv_text = File.read(Rails.root.join('lib', 'seeds', 'Exhibitions_test.csv'))
+  csv_text = File.read(Rails.root.join('lib', 'seeds', 'Exhibitions.csv'))
   csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
   csv.each do |row|
     t = Exhibition.new
