7c24ca1ba43a1d08193b0ec817c66514194891bb
Benjamin Leray <b.leray35@gmail.com>
2020-01-14 12:27:44 +0100

    Improve collapsing behavior w/ swipe previous element

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,8 +10,8 @@ class PagesController < ApplicationController
     # developpement exhibitions
     @temp_exhibitions = Exhibition.all
     # Production exhibitions
-    @personal_exhibitions = Exhibition.where(category: "Expositions personelles")
-    # @collective_exhibitions = Exhibition.where(category: "Expositions collectives")
-    # @permanent_exhibitions = Exhibition.where(category: "Collection publiques et privées")
+    @personal_exhibitions = Exhibition.where(category: "Expositions personelles").order(year: :desc)
+    # @collective_exhibitions = Exhibition.where(category: "Expositions collectives").order(year: :desc)
+    # @permanent_exhibitions = Exhibition.where(category: "Collection publiques et privées").order(year: :desc)
   end
 end
