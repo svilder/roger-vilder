fa45eba4a6a5823d3466fce32af3e4b69274bc72
Sandra Vilder <sandra.vilder@gmail.com>
2020-06-16 15:35:03 +0200

    validations on all models

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -2,4 +2,6 @@ class Exhibition < ApplicationRecord
   EXHIBITIONS_CATEGORIES = ["Collection publiques et privées", "Expositions personelles", "Expositions collectives"]
 
   validates :category, inclusion: { in: EXHIBITIONS_CATEGORIES }
+  validates :year, presence: true
+  validates :title, presence: true
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,2 +1,5 @@
 class Text < ApplicationRecord
+    validates :title_fr, presence: true
+    validates :author, presence: true
+    validates :content_fr, presence: true
 end
