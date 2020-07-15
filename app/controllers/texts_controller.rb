c8f4268257835e830a7d8db2ac804c5fbe25ecaf
Sandra Vilder <sandra.vilder@gmail.com>
2020-07-15 10:18:10 +0200

    texts in asc order

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,16 +1,16 @@
 class TextsController < ApplicationController
   def show
     if locale == :fr
-      @texts = Text.all.order(year: :desc)
-      @text = Text.all.order(year: :desc).first
+      @texts = Text.all.order(year: :asc)
+      @text = Text.all.order(year: :asc).first
       @text = Text.find(params[:id]) if params[:id]
       @content = @text.content_fr
       @title = @text.title_fr
 
 
     else
-      @texts = Text.all.order(year: :desc)
-      @text = Text.all.order(year: :desc).first
+      @texts = Text.all.order(year: :asc)
+      @text = Text.all.order(year: :asc).first
       @text = Text.find(params[:id]) if params[:id]
       @content = @text.content_en
       @title = @text.title_en
