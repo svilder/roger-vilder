c815297fef7a98d000c7a379af172a2193f33229
Sandra Vilder <sandra.vilder@gmail.com>
2020-07-17 12:18:58 +0200

    Gem particles assets wrapper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,5 @@
 import "bootstrap";
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', '/assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
