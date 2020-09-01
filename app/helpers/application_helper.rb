50b16a4e594a781fad630f1f5cbe6f9440ae1883
Sandra Vilder <sandra.vilder@gmail.com>
2020-09-01 14:19:24 +0200

    display ghost link arrow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
 
   def render_previous_work(works_hash, work)
     if works_hash[work] == 0
-      ""
+      render :partial => 'shared/no_work_link'
     else
       previous_work = works_hash.key(works_hash[work] - 1)
       render :partial => 'shared/previous_work_link', :locals => { :id => previous_work.id }
@@ -22,7 +22,7 @@ module ApplicationHelper
 
   def render_next_work(works_hash, work)
     if works_hash[work] == (works_hash.length - 1)
-      ""
+      render :partial => 'shared/no_work_link'
     else
       next_work = works_hash.key(works_hash[work] + 1)
       render :partial => 'shared/next_work_link', :locals => { :id => next_work.id }
