950c36b9898b6986f9d2044509f99ab87a50c200
Benjamin <b.leray35@gmail.com>
2023-02-27 22:37:52 +0100

    clean up, and seed action

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,15 @@ MODELS = %w[Work Text Exhibition Bibliography]
 
 strip = lambda {|f| f ? f.strip : nil}
 
-if Rails.env.development?
-  AdminUser.destroy_all
-  puts "Creating an admin user for testing active admin"
-  admin = AdminUser.new(email: "supertoto@gmail.com", password: "grostoto")
-  admin.save
+MODELS.each do |model|
+  puts "#{model.pluralize} cleaning"
+  model.constantize.destroy_all
+  puts "Generating #{model} data"
 
-  MODELS.each do |model|
-    puts "#{model.pluralize} cleaning"
-    model.constantize.destroy_all
-    puts "Generating #{model} data"
+  csv_text = File.read(Rails.root.join('lib',"seeds/#{model.underscore.pluralize}.csv"))
+  csv = CSV.parse(csv_text, headers: true, converters: strip)
 
-    csv_text = File.read(Rails.root.join('lib',"seeds/#{model.underscore.pluralize}.csv"))
-    csv = CSV.parse(csv_text, headers: true, converters: strip)
-
-    "#{model}Seed".constantize.new(csv).call
-    
-    puts "#{model.constantize.count} #{model.pluralize} has been seeded"
-  end
+  "#{model}Seed".constantize.new(csv).call
+  
+  puts "#{model.constantize.count} #{model.pluralize} has been seeded"
 end
