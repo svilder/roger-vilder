b6cb016b918ada2236c5d23bd995de07a7267c21
Benjamin <b.leray35@gmail.com>
2023-03-05 18:55:11 +0100

    clean up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,1 @@
 require("bootstrap");
-// assets/particles.json
-
-// if (document.getElementById('particles-js')) {
-//  particlesJS.load('particles-js', 'assets/particles.json', function() {
-//    console.log('callback - particles.js config loaded');
-//  });
-// };
