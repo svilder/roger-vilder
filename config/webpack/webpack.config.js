481a601f6ea22487abd69dc8652f680a9802c504
Benjamin <b.leray35@gmail.com>
2024-03-03 17:11:21 +0100

    wip

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,11 +1,32 @@
 const path    = require("path")
 const webpack = require("webpack")
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application:[
+      "./app/javascript/application.js",
+      "./app/assets/stylesheets/application.scss",
+    ]
+  },
+  module: {
+    rules: [
+      // Add CSS/SASS/SCSS rule with loaders
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    // Add additional file types
+    extensions: ['.js', '.jsx', '.scss', '.css'],
   },
   output: {
     filename: "[name].js",
@@ -15,6 +36,8 @@ module.exports = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
